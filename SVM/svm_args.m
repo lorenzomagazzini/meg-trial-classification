96155f8a959cedbda8f1d34999f22ebebc82aec7
c1465333 <dimadc@cardiff.ac.uk>
2017-11-20 16:37:42 +0000

    DD: Add iterated CV option

--- SVM/svm_args.m
+++ SVM/svm_args.m
@@ -6,6 +6,7 @@ classdef svm_args
         solver = 1; %liblinear: 1: L2 dual-problem; 2: L2 primal; 3:L2RL1L...
         boxconstraint = 1;
         kfold = 5;
+        iterate_cv = 1;
         standardize = true;
         weights = false;
        
@@ -19,8 +20,9 @@ classdef svm_args
                 obj.solver = varargin{1};
                 obj.boxconstraint = varargin{2};
                 obj.kfold = varargin{3};
-                obj.standardize = varargin{4};
-                obj.weights = varargin{5};
+                obj.iterate_cv = varargin{4};
+                obj.standardize = varargin{5};
+                obj.weights = varargin{6};
             end;
         end
                 
--- SVM/svm_decode_kfold_auc.m
+++ SVM/svm_decode_kfold_auc.m
@@ -2,20 +2,21 @@ function [ results ] = svm_decode_kfold_auc( data, labels, varargin )
 % Inputs: data(trials x features), labels. Optional: svm
 % parameters and desired optional performance metrics.
 % This function uses LibSVM and calculates posterior probabilities from SVM
-% scores to get AUC and ROC curve. It is slower than the "svm_decode_kfold" function 
-% (which uses the LibLinear implementation and does not output AUROC). 
-% Not recommended for large repetitions of classification iterations. 
-% Note that LibSVM and LibLinear can have somewhat different results even 
+% scores to get AUC and ROC curve. It is slower than the "svm_decode_kfold" function
+% (which uses the LibLinear implementation and does not output AUROC).
+% Not recommended for large repetitions of classification iterations.
+% Note that LibSVM and LibLinear can have somewhat different results even
 % when linear kernel is used (like here).
 %
-% Name-value pairs: 
+% Name-value pairs:
 %   'plotROC', default false (plot ROC curve).
 %   'boxconstraint', default 1
 %   'kfold', default 5
+%   'iterate_cv', default 1, number of cross-validation iterations
 %   'standardize', default true (recommended; across training set)
 %   'weights', default false (output vector of classifier weights & activation patterns associated with them cf. Haufe 2014)
 %
-% Outputs results structure with non-optional metrics: AUC, ROC, accuracy, Fscore, sensitivity, specificity. 
+% Outputs results structure with non-optional metrics: AUC, ROC, accuracy, Fscore, sensitivity, specificity.
 % Optional: weights and weight-derived patterns.
 % Basic function using libSVM implementation of svm for classification. Only implements kfold crossvalidation.
 
@@ -31,43 +32,51 @@ parse(p, varargin{:});
 svm_par = p.Results;
 clear p;
 
-if abs(nargin)<2 
+if abs(nargin)<2
     error('Data and labels are needed as inputs.')
 end;
 
-cv = cvpartition(labels, 'kfold', svm_par.kfold);
-allscore = zeros(length(labels),1); accuracy = zeros(3,5); post_prob=zeros(length(labels),2);
+results = struct;
 
-for ii = 1:svm_par.kfold
+for icv = 1: svm_par.iterate_cv
     
-    %scale values using range and minimum of training set
-    kdata = data; %ensures we keep original data
-    if svm_par.standardize
-        data = (kdata - repmat(min(data(cv.training(ii),:), [], 1), size(kdata, 1), 1)) ./ repmat(max(data(cv.training(ii),:), [], 1) - min(data(cv.training(ii),:), [], 1), size(kdata, 1), 1);
+    cv = cvpartition(labels, 'kfold', svm_par.kfold);
+    allscore = zeros(length(labels),1); accuracy = zeros(3,5); post_prob=zeros(length(labels),2);
+
+    for ii = 1:svm_par.kfold
+        
+        %scale values using range and minimum of training set
+        kdata = data; %ensures we keep original data
+        if svm_par.standardize
+            data = (kdata - repmat(min(data(cv.training(ii),:), [], 1), size(kdata, 1), 1)) ./ repmat(max(data(cv.training(ii),:), [], 1) - min(data(cv.training(ii),:), [], 1), size(kdata, 1), 1);
+        end;
+        
+        svm_model = svmtrain(labels(cv.training(ii)), data(cv.training(ii),:), sprintf('-t 0 -c %d -b 1 -q 1', svm_par.boxconstraint));
+        [allscore(cv.test(ii)), accuracy(:,ii), post_prob(cv.test(ii),:)] = svmpredict(labels(cv.test(ii)), data(cv.test(ii),:), svm_model, '-b 1 -q 1');
+        
     end;
     
-    svm_model = svmtrain(labels(cv.training(ii)), data(cv.training(ii),:), sprintf('-s 0 -t 0 -c %d -b 1 -q 1', svm_par.boxconstraint));
-    [allscore(cv.test(ii)), accuracy(:,ii), post_prob(cv.test(ii),:)] = svmpredict(labels(cv.test(ii)), data(cv.test(ii),:), svm_model, '-b 1 -q 1');
+    results.Accuracy(icv) = mean(accuracy(1,:));
+    results.AccuracyMSError(icv) = mean(accuracy(2,:));
+    results.AccuracyFold(icv,:) = accuracy(1,:);
+    results.Confusion{icv} = confusionmat(labels,allscore);
+    results.Sensitivity(icv) = results.Confusion{icv}(1,1)/(sum(results.Confusion{icv}(1,:))); %TP/allP = TP/(TP+FN)
+    results.Specificity(icv) = results.Confusion{icv}(2,2)/(sum(results.Confusion{icv}(2,:))); %TN/allN = TN/(FP+TN)
+    PP = results.Confusion{icv}(1,1)/(sum(results.Confusion{icv}(:,1))); %positive predictive value: class1
+    NP = results.Confusion{icv}(2,2)/(sum(results.Confusion{icv}(:,2))); %negative predictive value: class2
+    results.Fscore1(icv) = (2*PP*results.Sensitivity(icv))/(PP+results.Sensitivity(icv));
+    results.Fscore2(icv) = (2*NP*results.Specificity(icv))/(NP+results.Specificity(icv));
+    results.WeightedFscore(icv) = ((sum(results.Confusion{icv}(:,1))/sum(results.Confusion{icv}(:)))*results.Fscore1(icv)) + ((sum(results.Confusion{icv}(:,2))/sum(results.Confusion{icv}(:)))*results.Fscore2(icv));
+    [results.ROC{icv}(:,1),results.ROC{icv}(:,2),~,results.AUC(icv)] = perfcurve(labels, post_prob(:,1),'1');
     
-end;
-
-results.Accuracy = mean(accuracy(1,:));
-results.AccuracyMSError = mean(accuracy(2,:));
-results.AccuracyFold = accuracy(1,:);
-results.Confusion = confusionmat(labels,allscore);
-results.Sensitivity = results.Confusion(1,1)/(sum(results.Confusion(1,:))); %TP/allP = TP/(TP+FN)
-results.Specificity = results.Confusion(2,2)/(sum(results.Confusion(2,:))); %TN/allN = TN/(FP+TN)
-results.Fscore1 = (2*PP*results.Sensitivity)/(PP+results.Sensitivity);
-results.Fscore2 = (2*NP*results.Specificity)/(NP+results.Specificity);
-results.WeightedFscore = ((sum(results.Confusion(:,1))/sum(results.Confusion(:)))*results.Fscore1) + ((sum(results.Confusion(:,2))/sum(results.Confusion(:)))*results.Fscore2);
-[results.ROC(:,1),results.ROC(:,2),~,results.AUC] = perfcurve(labels, post_prob(:,1),'1');
-
-if svm_par.plotROC
-    figure;
-    plot(results.ROC(:,1),results.ROC(:,2), 'k');
-    xlabel('False positive rate');
-    ylabel('True positive rate');
-    title('ROC Curve', 'FontWeight', 'normal');
+    if svm_par.plotROC
+        figure;
+        plot(results.ROC{icv}(:,1),results.ROC{icv}(:,2), 'k');
+        xlabel('False positive rate');
+        ylabel('True positive rate');
+        title('ROC Curve', 'FontWeight', 'normal');
+    end;
+    
 end;
 
 %calculate weights and compute activation patterns as per Haufe (2014)
