1bcfa62f2c7e8a39414c385ec23c44b5238f80b0
c1356674 <magazzinil@cardiff.ac.uk>
2018-04-19 16:39:06 +0100

    remove trial loop, use cat in 2 lines of code

--- trialfeatures/get_wthn_chan_variance.m
+++ trialfeatures/get_wthn_chan_variance.m
@@ -1,33 +1,14 @@
-function [ output_args ] = get_wthn_chan_variance( data )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ wthn_chan_var ] = get_wthn_chan_variance( data )
+%[ wthn_chan_var ] = get_wthn_chan_variance( data )
+%   Calculate within-channel variance, separately for each trial.
+%   Input:
+%       data: struct, with field data.trial (1 x NTrl cell array).
+%   Output:
+%       wthn_chan_var: NChan x NTrl matrix.
 
-%store trials into 3D matrix (good and bad trials distinguished later)
+%store trials in 3D matrix (good and bad trials distinguished later)
 trl = cat(3, data.trial{:});
 
-nTrial = size(trl,3);
-chanvarsum_arr = nan(1,nTrial);
-chanvarmax_arr = nan(1,nTrial);
-for iTrial = 1: nTrial
-    
-    %select single trial
-    chantimematrix = squeeze(trl(:,:,1));
-%     figure, imagesc(chantimematrix)
-    
-    %calculate within-channel variance
-    chanvar = var(chantimematrix,[],2);
-    
-    %sum variance over channels
-    chanvarsum = sum(chanvar);
-    
-    %max variance across trials
-    chanvarmax = max(chanvar);
-    
-    %store in array
-    chanvarsum_arr(iTrial) = chanvarsum;
-    chanvarmax_arr(iTrial) = chanvarmax;
-    
-end
-
-end
+%calculate within-channel variance, separately for each trial (NChan x NTrl)
+wthn_chan_var = squeeze(var(trl,[],2));
 
