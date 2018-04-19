6cc8bf0e0b07da6df95c38c57a8235ca1a54ab17
c1356674 <magazzinil@cardiff.ac.uk>
2018-04-19 18:24:46 +0100

    add between channel variance function

---
+++ trialfeatures/get_btwn_chan_variance.m
@@ -0,0 +1,14 @@
+function [ btwn_chan_var ] = get_btwn_chan_variance( data )
+%[ btwn_chan_var ] = get_btwn_chan_variance( data )
+%   Calculate between-channel variance, separately for each trial.
+%   Input:
+%       data: struct, with field data.trial (1 x NTrl cell array).
+%   Output:
+%       btwn_chan_var: NTrl x NTime matrix.
+
+%store trials in 3D matrix (good and bad trials distinguished later)
+trl = cat(3, data.trial{:});
+
+%calculate variance across channels, separately for each trial (NTrl x NTime)
+btwn_chan_var = squeeze(var(trl,[],1));
+
