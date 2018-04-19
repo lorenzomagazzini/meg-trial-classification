d4d198b023c59306a50f1320ecac527e965e1ccf
c1356674 <magazzinil@cardiff.ac.uk>
2018-04-19 16:29:02 +0100

    use cat for 3D trial matrix

--- trialfeatures/get_wthn_chan_variance.m
+++ trialfeatures/get_wthn_chan_variance.m
@@ -2,14 +2,16 @@ function [ output_args ] = get_wthn_chan_variance( data )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-%loop over all trials, distinguish good and rejected trials later
-nTrial = length(data.trial);
+%store trials into 3D matrix (good and bad trials distinguished later)
+trl = cat(3, data.trial{:});
+
+nTrial = size(trl,3);
 chanvarsum_arr = nan(1,nTrial);
 chanvarmax_arr = nan(1,nTrial);
 for iTrial = 1: nTrial
     
     %select single trial
-    chantimematrix = data.trial{iTrial};
+    chantimematrix = squeeze(trl(:,:,1));
 %     figure, imagesc(chantimematrix)
     
     %calculate within-channel variance
