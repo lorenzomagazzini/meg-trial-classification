48d6fbc2c075f0f92b2323d884661e4e661684ba
c1356674 <magazzinil@cardiff.ac.uk>
2018-04-24 14:05:09 +0100

    take abs correlation because of magnetic field pos/neg stuff

--- trialfeatures/get_chan_correlation.m
+++ trialfeatures/get_chan_correlation.m
@@ -1,6 +1,6 @@
 function [ chan_corr ] = get_chan_correlation( data )
 %   Calculate each channel's mean correlation with all other channels, 
-%   separately for each trial.
+%   separately for each trial. Ignore direction of correlation (take abs).
 %   Input:
 %       data: struct, with field data.trial (1 x NTrl cell array).
 %   Output:
@@ -11,7 +11,7 @@ chan_corr = nan(size(trl,1),size(trl,3),1);
 
 for t = 1:size(trl,3)
     rho = corr(trl(:,:,t)');
-    chan_corr(:,t) = mean(rho,1);
+    chan_corr(:,t) = mean(abs(rho),1);
 end;
 
 
