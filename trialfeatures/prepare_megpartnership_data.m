a87e9001228e82565c7645cce96b2247febb007b
c1356674 <magazzinil@cardiff.ac.uk>
2018-05-22 13:55:16 +0100

    rewrite script, use bids data instead of partnership, no preprocessing for raw data

--- trialfeatures/prepare_megpartnership_data.m
+++ trialfeatures/prepare_megpartnership_data.m
@@ -3,6 +3,7 @@
 addpath('/home/c1356674/git-lab/Trial-classification/trialfeatures/')
 addpath('/home/c1356674/git-lab/ampreject/functions')
 addpath('/cubric/collab/meg-partnership/git-lab/meguk-data-management/meguk_anonym')
+addpath('/cubric/collab/meg-partnership/git-lab/meguk-data-analysis')
 
 %CUBRIC paths:
 addpath('/cubric/software/MEG/CUBRIC/FileIO/ctf/')
@@ -10,154 +11,83 @@ addpath('/cubric/software/MEG/CUBRIC/FileIO/ctf/')
 
 clear
 
-%path to MEG Partnership data in collab
-base_path = '/cubric/collab/meg-partnership/Analysis';
+%path to MEGUK BIDS dataset
+bids_path = '/cubric/collab/meg-partnership/cardiff/bids';
 
-%path to save data temporarily, before copying to final destination path
-temp_savepath = '/cubric/scratch/c1356674/meg-cleaning/tempdata';
-if ~exist(temp_savepath,'dir'), mkdir(temp_savepath); end
-unix(['chmod -R 700 ' temp_savepath])
-unix('chmod 700 /cubric/scratch/c1356674/meg-cleaning/')
-
-%path to save data and bad trials .mat files
-data_savepath = '/cubric/collab/meg-cleaning/megpartnership/restopen/traindata';
+%path to save raw data
+base_path = '/cubric/collab/meg-cleaning/megpartnership/restopen';
+data_savepath = fullfile(base_path, 'traindata');
 if ~exist(data_savepath,'dir'), mkdir(data_savepath); end
-unix(['chmod -R 700 ' data_savepath])
-unix('chmod 700 /cubric/collab/meg-cleaning/megpartnership/restopen/')
-unix('chmod 700 /cubric/collab/meg-cleaning/megpartnership/')
 
 %path to save figures
-fig_savepath = '/cubric/collab/meg-cleaning/megpartnership/restopen/trainfigures';
+fig_savepath = fullfile(base_path, 'trainfigures');
 if ~exist(fig_savepath,'dir'), mkdir(fig_savepath); end
-unix(['chmod -R 700 ' fig_savepath])
+
+%set permissions
+unix(['chmod -R 770 ' base_path])
+unix(['addWriteAccessTo 1205074 ' base_path])
+unix(['addWriteAccessTo 1205074 ' fullfile(base_path, 'traindata')])
+unix(['addWriteAccessTo 1205074 ' fullfile(base_path, 'trainfigures')])
 
 
-%% get list of participants
+%% define list of MEGUK IDs
 
-cd(base_path)
-tmp_dir = dir('7*');
-
-subj_list = {tmp_dir(:).name}';
-clear tmp*
-
-nsubj = length(subj_list);
+id_list = define_megpartnership_ids;
+nsubj = length(id_list);
 
 
-%% read data and ClassFile
+%% get raw resteyesopen data from BIDS directory
 
-%definitions for filtering/plotting
-hp_freq = 100;
-lp_freq = 4;
+cfg = [];
+cfg.total_duration = 300; %in seconds
+cfg.trial_duration = 2; %in seconds
+cfg.anonymise = 'no';
 
-%every other subject, starting from first subj (training data)
-for s = 1:2:nsubj
+for s = 1:nsubj %missing MarkerFile: 8, 17, 20, 25, 30, 33, 36
     
+    %define subject, dataset, markerfile
+    subj = ['sub-' id_list{s}];
+    cfg.dataset = fullfile(bids_path, subj, 'meg', [subj '_task-resteyesopen_meg.ds']);
+    markerfile = fullfile(cfg.dataset, 'MarkerFile.mrk');
     
-    %subject ID and dataset
-    subj = subj_list{s};
-    tmp_dir = dir(fullfile(base_path, subj, [subj '*RestO*']));
-    dataset = fullfile(base_path, subj, tmp_dir.name);
-    clear tmp*
+    %find marker (if MarkerFile exists)
+    if exist(markerfile,'file')
+        
+        mrk = readmarkerfile(cfg.dataset);
+        if mrk.number_markers == 1
+            cfg.eventtype = mrk.marker_names{1};
+        else
+            error('multiple markers found')
+        end
+        
+	%otherwise, try reading events (for example, sub-cdf067)
+    else
+        
+        evt = ft_read_event(cfg.dataset);
+        if isempty(evt)
+            cfg.eventtype = '';
+            cfg.eventvalue = [];
+        else
+            cfg.eventtype = 'UPPT002';
+            evt_index = find(ismember({evt(:).type}, cfg.eventtype)); %first trigger is taken, in case of multiple UPPT002 events
+            cfg.eventvalue = evt(evt_index(1)).value;
+        end
+        
+    end
     
+    %epoch dataset (no preprocessing such as filtering or demeaning, at this stage)
+    [data_epoc, data_cont] = epoch_resteyesopen(cfg);
     
-    %raw data, high-pass filtered
-    cfg = [];
-    cfg.dataset = dataset;
-    cfg.channel = {'MEG'};
-    cfg.hpfilter = 'yes';
-    cfg.hpfreq = 1;
-    data = ft_preprocessing(cfg)
+    %save continuous .mat file
+    [~, dataset_name] = fileparts(cfg.dataset);
+    cont_savename = [dataset_name '-continuous.mat'];
+    fprintf('saving file %s to %s\n', cont_savename, data_savepath)
+    save(fullfile(data_savepath, cont_savename), '-v7.3', '-struct', 'data_cont')
     
-    %anonymise data
-    data.cfg.dataset = '';
-    data.cfg.datafile = '';
-    data.cfg.headerfile = '';
-    data.cfg.callinfo = [];
-    
-    
-    %index to bad trials
-    classfile = fullfile(dataset, 'ClassFile.cls');
-    trialclass = readClassFile(classfile);
-    badtrialsindex = trialclass.trial
-    
-    %boolean bad trials array
-    badtrialsboolean = zeros(1,length(data.trial));
-    badtrialsboolean(badtrialsindex) = 1;
-    
-    
-    %save data to temporary path
-    id = partnership2meguk(subj, '/cubric/collab/meg-partnership/cardiff/private/id_conversion_cardiff.mat');
-    data_savename = ['sub-' id '_data.mat'];
-    save(fullfile(temp_savepath, data_savename), '-v7.3', '-struct', 'data')
-    
-    %save bad trials to temporary path
-    badtrials_savename = ['sub-' id '_cls-bethvisual.mat'];
-    save(fullfile(temp_savepath, badtrials_savename), '-v7.3', 'badtrialsindex', 'badtrialsboolean')
-    
-    
-    close all
-    
-    %plot bad trials
-    cfg = [];
-    cfg.yLim = [-1 1]*5e-12;
-    cfg.title = 'Visually rejected trials';
-    cfg.drawnow = 'no';
-    [~, hFigure1] = amprej_multitrialplot(cfg, data, badtrialsboolean);
-    drawnow
-    pause(1)
-    
-    %save figure to temporary path
-    fig_savename = ['sub-' id '_cls-bethvisual_broadband.png'];
-    saveas(hFigure1, fullfile(temp_savepath, fig_savename))
-    
-    
-    %high-pass filter data
-    cfg = [];
-    cfg.hpfilter = 'yes';
-    cfg.hpfreq = hp_freq;
-    data_hp = ft_preprocessing(cfg, data)
-    
-    %plot bad trials
-    cfg = [];
-    cfg.yLim = [-1 1]*5e-12;
-    cfg.title = ['Visually rejected trials (high-pass ' num2str(hp_freq) ' Hz)'];
-    cfg.drawnow = 'no';
-    [~, hFigure2] = amprej_multitrialplot(cfg, data_hp, badtrialsboolean);
-    drawnow
-    pause(1)
-    
-    %save figure to temporary path
-    fig_savename = ['sub-' id '_cls-bethvisual_hp-' num2str(hp_freq) 'Hz.png'];
-    saveas(hFigure2, fullfile(temp_savepath, fig_savename))
-    
-    
-    %low-pass filter data
-    cfg = [];
-    cfg.lpfilter = 'yes';
-    cfg.lpfreq = lp_freq;
-    data_lp = ft_preprocessing(cfg, data)
-    
-    %plot bad trials
-    cfg = [];
-    cfg.yLim = [-1 1]*5e-12;
-    cfg.title = ['Visually rejected trials (low-pass ' num2str(lp_freq) ' Hz)'];
-    cfg.drawnow = 'no';
-    [~, hFigure3] = amprej_multitrialplot(cfg, data_lp, badtrialsboolean);
-    drawnow
-    pause(1)
-    
-    %save figure to temporary path
-    fig_savename = ['sub-' id '_cls-bethvisual_lp-' num2str(lp_freq) 'Hz.png'];
-    saveas(hFigure3, fullfile(temp_savepath, fig_savename))
-    
+    %save epoched .mat file
+    epoc_savename = [dataset_name '-epoched.mat'];
+    fprintf('saving file %s to %s\n', cont_savename, epoc_savename)
+    save(fullfile(data_savepath, epoc_savename), '-v7.3', '-struct', 'data_epoc')
     
 end
-
-%copy data from  scratch to collab
-unix(['cp -v ' fullfile(temp_savepath, 'sub-*.mat') ' ' data_savepath])
-unix(['cp -v ' fullfile(temp_savepath, 'sub-*.png') ' ' fig_savepath])
-
-%remove data from scratch
-unix(['rm -rv ' temp_savepath])
-
 
