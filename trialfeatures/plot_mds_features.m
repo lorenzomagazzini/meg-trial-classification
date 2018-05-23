2b366bcc95b4e410339d62a7dbac3a5d777b6abb
c1465333 <DimaDC@cardiff.ac.uk>
2018-05-23 16:03:47 +0100

    add MDS plots

---
+++ trialfeatures/plot_mds_features.m
@@ -0,0 +1,30 @@
+function [ ] = plot_mds_features( features, trl_idx )
+% Multi-dimensional scaling plots 
+
+for i = 1:length(features)
+    
+    fields = fieldnames(features(i));
+    exclude_features = {'chan_corr','wthn_chan_var','btwn_chan_var','wthn_chan_kurt','btwn_chan_kurt'}; %we only plot summary ones (otherwise takes ages to get distances)
+    fields(ismember(fields,exclude_features)) = []; 
+    
+    subplot_n = ceil(length(fields)/3);
+   
+    figure;
+      
+    for f = 1:length(fields)
+        
+        feature = getfield(features(i), fields{f});
+        Y = mdscale(squareform(pdist(feature(:))),2, 'criterion', 'strain', 'start', 'random'); %non-classical MDS with a criterion similar to that used in classical MDS, with random locations
+    
+        subplot(3,subplot_n,f); 
+        plot(Y(trl_idx==1,1), Y(trl_idx==1,2), 'or', 'markerfacecolor', 'r','markersize', 3); hold on; %bad trl in red
+        plot(Y(trl_idx==0,1), Y(trl_idx==0,2), 'sb', 'markerfacecolor', 'b','markersize', 3); hold on;
+        title(fields{f});
+    end
+    
+end
+    
+
+
+
+end
