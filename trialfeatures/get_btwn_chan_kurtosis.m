7598cf8355c08990f3365da3936d98f7e1787f52
c1356674 <magazzinil@cardiff.ac.uk>
2018-06-01 13:40:01 +0100

    update feature functions to work with sliding window approach

--- trialfeatures/get_btwn_chan_kurtosis.m
+++ trialfeatures/get_btwn_chan_kurtosis.m
@@ -1,13 +1,37 @@
-function [ btwn_chan_kurt ] = get_btwn_chan_kurtosis( data )
+function [ btwn_chan_kurt ] = get_btwn_chan_kurtosis( data, slide_window )
+%[ btwn_chan_kurt ] = get_btwn_chan_kurtosis( data, slide_window )
 %   Calculate between-channel kurtosis, separately for each trial.
 %   Input:
-%       data: struct, with field data.trial (1 x NTrl cell array).
+%       data:           struct, with field data.trial (1 x NTrl cell array), when slide_window is false.
+%                       NChan x 1 x NTrl matrix, when optional input slide_window is true.
+%       slide_window:   logical, optional input determining the dimension of the output matrix wthn_chan_var (default = false).
 %   Output:
-%       wthn_chan_kurt: NTime x NTrl matrix.
+%       wthn_chan_kurt: NTime x NTrl matrix, when slide_window is false.
+%                       NTime x 1 x NTrl matrix, when slide_window is true.
 
-trl = cat(3, data.trial{:});
-btwn_chan_kurt = squeeze(kurtosis(trl,[],1)); %kurtosis along time axis
+% Written by Diana Dima
+% Re-written in May 2018 by Lorenzo Magazzini (magazzinil@gmail.com)
 
+%%
 
+%do not slide window by default
+if nargin<2 || isempty(slide_window)
+    slide_window = false;
+else
+    slide_window = istrue(slide_window);
+end
+
+%return 3D matrix for sliding window approach
+if slide_window
+    
+    trl = data; %data is already a 3D matrix, not a structure
+    btwn_chan_kurt(:,1,:) = permute(kurtosis(trl,[],1),[2 1 3]); %kurtosis over channels dimension
+    
+%return 2D matrix otherwise
+elseif ~slide_window
+    
+    trl = cat(3, data.trial{:}); %store trials in 3D matrix
+    btwn_chan_kurt = kurtosis(trl,[],1); %calculate between-channel kurtosis, separately for each trial (NTime x NTrl)
+    
 end
 
--- trialfeatures/get_btwn_chan_variance.m
+++ trialfeatures/get_btwn_chan_variance.m
@@ -1,14 +1,37 @@
-function [ btwn_chan_var ] = get_btwn_chan_variance( data )
-%[ btwn_chan_var ] = get_btwn_chan_variance( data )
+function [ btwn_chan_var ] = get_btwn_chan_variance( data, slide_window )
+%[ btwn_chan_var ] = get_btwn_chan_variance( data, slide_window )
 %   Calculate between-channel variance, separately for each trial.
 %   Input:
-%       data: struct, with field data.trial (1 x NTrl cell array).
+%       data:           struct, with field data.trial (1 x NTrl cell array), when slide_window is false.
+%                       NChan x 1 x NTrl matrix, when optional input slide_window is true.
+%       slide_window:   logical, optional input determining the dimension of the output matrix wthn_chan_var (default = false).
 %   Output:
-%       btwn_chan_var: NTime x NTrl matrix.
+%       btwn_chan_var:  NTime x NTrl matrix, when slide_window is false.
+%                       NTime x 1 x NTrl matrix, when slide_window is true.
 
-%store trials in 3D matrix (good and bad trials distinguished later)
-trl = cat(3, data.trial{:});
+% Written by Lorenzo Magazzini (magazzinil@gmail.com)
+% Re-written in May 2018
 
-%calculate variance across channels, separately for each trial (NTime x NTrl)
-btwn_chan_var = squeeze(var(trl,[],1));
+%%
+
+%do not slide window by default
+if nargin<2 || isempty(slide_window)
+    slide_window = false;
+else
+    slide_window = istrue(slide_window);
+end
+
+%return 3D matrix for sliding window approach
+if slide_window
+    
+    trl = data; %data is already a 3D matrix, not a structure
+    btwn_chan_var(:,1,:) = permute(var(trl,[],1),[2 1 3]); %variance across channels
+    
+%return 2D matrix otherwise
+elseif ~slide_window
+    
+    trl = cat(3, data.trial{:}); %store trials in 3D matrix
+    btwn_chan_var = squeeze(var(trl,[],1)); %calculate variance across channels, separately for each trial (NTime x NTrl)
+    
+end
 
