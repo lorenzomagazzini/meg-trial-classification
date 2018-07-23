9f10f142c4f377538ce92889eac554b499ebcef9
c1356674 <magazzinil@cardiff.ac.uk>
2018-07-23 15:19:39 +0100

    update function help

--- trialfeatures/feat_extract_func/get_hurst_exponent.m
+++ trialfeatures/feat_extract_func/get_hurst_exponent.m
@@ -1,23 +1,22 @@
 function [ hexp ] = get_hurst_exponent( data )
 %   Calculate the timeseries Hurst exponent separately for each trial and channel. 
-%   Uses the discrete second derivative estimator as implemented in
-%   Matlab's Wavelet Toolbox.
+%   Uses the discrete second derivative estimator as implemented in Matlab's Wavelet Toolbox.
 %   Input:
 %       data: struct, with field data.trial (1 x NTrl cell array).
 %   Output:
 %       hexp: NChan x NTrl matrix.
 
-trl = cat(3, data.trial{:});
+% Written by Diana Dima (diana.dima@gmail.com)
+
+%%
+
+trl = cat(3, data.trial{:}); %store trials in 3D matrix
 hexp = nan(size(trl,1), size(trl,3)); %initialize NChan x NTrl matrix
 
 for t = 1:size(trl,3)
     for c = 1:size(trl,1)
         h = wfbmesti(squeeze(trl(c,:,t)));
         hexp(c,t) = h(1);
-    end;
-end;
-
-
-
+    end
 end
 
