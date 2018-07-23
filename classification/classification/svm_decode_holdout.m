d061e87549ead98e7b3a930d1659fa09edc30f5a
c1465333 <DimaDC@cardiff.ac.uk>
2018-07-23 18:24:46 +0100

    update & documentation

--- classification/classification/svm_decode_holdout.m
+++ classification/classification/svm_decode_holdout.m
@@ -1,4 +1,6 @@
 function results = svm_decode_holdout (train_data, train_labels, test_data, test_labels, varargin)
+% Basic classification function using LibLinear SVM implementation for classification and independent (pre-specified) training and test sets.
+%
 % Inputs: training and testing data(trials x features), training and testing abels. 
 % Optional: svm parameters and desired optional performance metrics.
 %
@@ -7,10 +9,12 @@ function results = svm_decode_holdout (train_data, train_labels, test_data, test
 % 'boxconstraint', default 1
 % 'standardize', default true (recommended; across training set)
 % 'weights', default false (output vector of classifier weights & activation patterns associated with them cf. Haufe 2014)
+% 
 %
 % Outputs results structure with non-optional metrics: accuracy, Fscore, sensitivity, specificity.
 % Optional (as above): weights and weight-derived patterns. (Set 'weights',true)
-% Basic function using LibLinear implementation of svm for classification, using independent training and test sets.
+%
+% DC Dima 2018 (diana.dima@gmail.com)
 
 %parse inputs
 svm_par = svm_args;
@@ -23,11 +27,11 @@ parse(p, varargin{:});
 svm_par = p.Results;
 clear p;
 
-if abs(nargin)<4
+if abs(nargin)<4 %at least 4 arguments needed
     error('Separate training and testing data and labels are needed as inputs.')
 end;
 
-%LibSVM requires double data
+%double precision data is required by LibLinear
 if ~isa(train_data, 'double')
     train_data = double(train_data);
 end;
@@ -35,7 +39,7 @@ if ~isa(test_data, 'double')
     test_data = double(test_data);
 end;
 
-%scale values using range and minimum of training set - this is best practice
+%scale values using range and minimum of training set 
 if svm_par.standardize
     train_data = (train_data - repmat(min(train_data, [], 1), size(train_data, 1), 1)) ./ repmat(max(train_data, [], 1) - min(train_data, [], 1), size(train_data, 1), 1);
     test_data = (test_data - repmat(min(train_data, [], 1), size(test_data, 1), 1)) ./ repmat(max(train_data, [], 1) - min(train_data, [], 1), size(test_data, 1), 1);
@@ -45,23 +49,22 @@ end;
 svm_model = train(train_labels, sparse(train_data), sprintf('-s %d -c %d -q 1', svm_par.solver, svm_par.boxconstraint));
 [scores, accuracy, ~] = predict(test_labels, sparse(test_data), svm_model, '-q 1'); %this outputs labels, accuracy, and standard error
 
-%put everything we can think of in a results structure
-results.Accuracy = accuracy(1);
-results.AccuracyMSError = accuracy(2);
-results.Confusion = zeros(2,2);
+%add performance metrics to a scalar results structure
+results.Accuracy = accuracy(1); %accuracy
+results.AccuracyMSError = accuracy(2); %accuracy mean squared error
 results.Confusion = confusionmat(test_labels,scores); %get the confusion matrix
-if numel(results.Confusion)>1 %check that this was output right (sometimes weird class numbers interfere with this)
+if numel(results.Confusion)>1 %check that this was output right first
     results.Sensitivity = results.Confusion(1,1)/(sum(results.Confusion(1,:))); %TP/allP = TP/(TP+FN)
     PP = results.Confusion(1,1)/(sum(results.Confusion(:,1))); %positive predictive value: class1
     results.Specificity = results.Confusion(2,2)/(sum(results.Confusion(2,:))); %TN/allN = TN/(FP+TN)
     NP = results.Confusion(2,2)/(sum(results.Confusion(:,2))); %negative predictive value: class2
-    results.Fscore1 = (2*PP*results.Sensitivity)/(PP+results.Sensitivity);
-    results.Fscore2 = (2*NP*results.Specificity)/(NP+results.Specificity);
-    results.WeightedFscore = ((sum(results.Confusion(:,1))/sum(results.Confusion(:)))*results.Fscore1) + ((sum(results.Confusion(:,2))/sum(results.Confusion(:)))*results.Fscore2);
+    results.Fscore1 = (2*PP*results.Sensitivity)/(PP+results.Sensitivity); %F1-score: class 1
+    results.Fscore2 = (2*NP*results.Specificity)/(NP+results.Specificity); %F1-score: class 2
+    results.WeightedFscore = ((sum(results.Confusion(:,1))/sum(results.Confusion(:)))*results.Fscore1) + ((sum(results.Confusion(:,2))/sum(results.Confusion(:)))*results.Fscore2); %F1-score weighted by class size
     results.Label = svm_model.Label; %save class order
 end;
 
-%calculate weights and compute activation patterns as per Haufe (2014)
+%calculate weights and compute activation patterns as per Haufe et al. (2014)
 if svm_par.weights
     results.Weights = svm_model.w;
     results.WeightPatterns = abs(cov(train_data)*results.Weights'/cov(train_data*results.Weights'));
--- classification/classification/svm_train.m
+++ classification/classification/svm_train.m
@@ -1,29 +1,32 @@
 function svm_model = svm_train (train_data, train_labels, varargin)
+% Basic classification function using LibLinear implementation, trains a linear SVM and returns trained svm model.
+%
 % Inputs: training data(trials x features), training labels. 
 % Optional: svm parameters and desired optional performance metrics.
 %
 % Name-value pairs: 
 % 'solver', default 1: L2-regularized dual problem solver (LibLinear);
 % 'boxconstraint', default 1
-% 'standardize', default true (recommended; across training set)
-% 'weights', default false (output vector of activation patterns associated with classifier weights, cf. Haufe 2014)
+% 'standardize', default true (recommended; based on training set)
+% 'weights', default false (output vector of activation patterns associated with classifier weights, cf. Haufe et al. 2014)
 %
 % Outputs svm model.
 % Optional (as above): weight-derived patterns. (Raw weights are always output in svm_model.w)
-% Basic function using LibLinear implementation, trains a linear SVM and returns trained svm model.
+%
+% DC Dima 2018 (diana.dima@gmail.com)
 
 %parse inputs
-svm_par = svm_args;
-list = fieldnames(svm_par);
-p = inputParser;
-for ii = 1:length(properties(svm_args))
+svm_par = svm_args; %define an object containing default parameters
+list = fieldnames(svm_par); 
+p = inputParser; 
+for ii = 1:length(properties(svm_args)) %create a list of parameters to parse, plus their defaults
     addParameter(p, list{ii}, svm_par.(list{ii}));
 end;
-parse(p, varargin{:});
+parse(p, varargin{:}); %check the name-value pairs in input and overwrite defaults where needed
 svm_par = p.Results;
 clear p;
 
-if abs(nargin)<2
+if abs(nargin)<2 %at least 2 arguments needed
     error('Data and labels are needed as inputs.')
 end;
 
@@ -33,13 +36,13 @@ if svm_par.standardize
     
 end;
 
+%train classifier using parameters provided
 svm_model = train(train_labels, sparse(train_data), sprintf('-s %d -c %d -q 1', svm_par.solver, svm_par.boxconstraint));
 
-
-%calculate weights and compute activation patterns as per Haufe (2014)
+%calculate weights and compute activation patterns as per Haufe et al. (2014)
 if svm_par.weights
     svm_model.WeightPatterns = abs(cov(train_data)*results.Weights'/cov(train_data*results.Weights'));
-    svm_model.WeightPatternsNorm = (results.WeightPatterns-min(results.WeightPatterns))/(max(results.WeightPatterns)-min(results.WeightPatterns));
+    svm_model.WeightPatternsNorm = (results.WeightPatterns-min(results.WeightPatterns))/(max(results.WeightPatterns)-min(results.WeightPatterns)); %normalized weights
 end;
 
 end
